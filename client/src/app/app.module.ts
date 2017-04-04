ff886524e346f73a4e08b6429dea5f99c0491f34
JulParman <julian.parman@gmail.com>
2017-04-04 15:01:50 +0300

    style modules added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact/contact-list/contact-list.component';
@@ -17,7 +18,9 @@ import { ContactListItemComponent } from './contact/contact-list/contact-list-it
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
